23e64c5de5823f8384157e83b2d7fe114bf6f151
UmairHashmi <umairhashmi1990@outlook.com>
2023-08-27 20:39:15 +0500

    Customized  Chart added in dashboard

--- SilkFlo.Web/Controllers/Test.cs
+++ SilkFlo.Web/Controllers/Test.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.JsonPatch.Internal;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
+using Newtonsoft.Json;
 using SilkFlo.Data.Core.Domain;
 using SilkFlo.Data.Persistence;
+using SilkFlo.Web.Models.Charts;
+using Stripe;
+using SVGChartTools.DataSet;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SilkFlo.Web.Controllers
 {
@@ -16,14 +27,14 @@ namespace SilkFlo.Web.Controllers
             return View();
         }
 
-       public IActionResult End()
+        public IActionResult End()
         {
             return View("Subscription/_EndNotification");
-        }  
+        }
         public IActionResult gracePeriod()
         {
             return View("Subscription/GracePeriod");
-        } 
+        }
         public IActionResult pre2day()
         {
             return View("Subscription/PreEnd2day");
@@ -36,7 +47,333 @@ namespace SilkFlo.Web.Controllers
         {
             return View("Subscription/PreEndLast");
         }
-    }
 
-   
+        public TypeofChart getTypeofCharts()
+        {
+            TypeofChart typeofChart = null;
+            //string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            //string jsonFilePath = Path.Combine(basePath, "Views", "Shared", "Charts", "Data", "ChartType.json");
+
+
+            //try
+            //{
+            //    string jsonContent = System.IO.File.ReadAllText(jsonFilePath);
+            //   typeofChart = JsonConvert.DeserializeObject<TypeofChart>(jsonContent);
+
+               
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"An error occurred: {ex.Message}");
+            //}
+
+            //
+
+
+
+            typeofChart = new TypeofChart()
+            {
+                lineChart = true,
+                barChart = true,
+                pieChart = true,
+                bubbleChart = false,
+                doughnutChart = true,
+                scatterChart = true,
+                polarChart = false,
+            };
+            return typeofChart;
+        }
+
+
+        public IActionResult Dashboard()
+        {
+            var charts = new vmCharts();
+            TypeofChart bolTypeofChart = getTypeofCharts();
+            if (bolTypeofChart.lineChart)
+            {
+                charts.lineChart = CreateLineChart("lineChart");
+            }
+            if (bolTypeofChart.barChart)
+            {
+                charts.barChart = CreateBarChart("barChart");
+            }
+            if (bolTypeofChart.pieChart)
+            {
+                charts.pieChart = CreatePieChart("pieChart");
+            }
+            if (bolTypeofChart.doughnutChart)
+            {
+                charts.doughnutChart = CreateDoughnutChart("doughnutChart");
+            }
+            if (bolTypeofChart.bubbleChart)
+            {
+                charts.bubbleChart = CreateBubbleChart("bubbleChart");
+            }
+            if (bolTypeofChart.polarChart)
+            {
+                charts.polarChart = CreatePolarChart("polarChart");
+            }
+            if (bolTypeofChart.scatterChart)
+            {
+                charts.scatterChart = CreateScatterChart("scatterChart");
+            }
+            
+           
+           
+           
+           
+           
+           
+          
+            return View("~/Views/Shared/Charts/Dashboard.cshtml" , charts);
+        }
+
+        private vmChart CreateLineChart(string ChartName)
+        {
+            var yAxesTicks = new ChartTicks { BeginAtZero = true };
+            var xAxesTicks = new ChartTicks { BeginAtZero = false };
+
+            var yAxesScale = new ChartScale { Ticks = yAxesTicks };
+            var xAxesScale = new ChartScale { Ticks = xAxesTicks };
+
+            
+            var lineChart = new vmChart
+            {
+                ChartName = ChartName,
+                strType = "line",
+                Height = "300",
+                Width = "250",
+                Responsive = true,
+                Animation = true,
+                Legend  = new ChartLegend
+                {
+                    Position= "bottom"
+                },
+                Labels = { "2000", "2005", "2010", "2015", "2020" },
+                data = new List<ChartData>
+                {
+                    new ChartData
+                    {
+                        //Label = "Deploved",
+                        Data = new List<double> { 30, 50, 20, 70, 40 },
+                        BackgroundColor = new List<string> { "rgba(255, 99, 132, 0.2)" },
+                        BorderColor = new List<string> { "rgb(255, 99, 132)" },
+                        BorderWidth = 1,
+                        Fill = false,
+                        Tension = Convert.ToDecimal( 0.1),
+                        Events=new List<string>{ "click" },
+                        ScaleShowVaEues=true,
+                        Scales=new ChartAxes  {
+                            yAxes = new List<ChartScale> { yAxesScale },
+                            xAxes = new List<ChartScale> { xAxesScale }
+                        },
+
+                    },
+                    new ChartData
+                    {
+                        //Label = "Estimated",
+                        Data = new List<double> { 50, 10, 30, 50, 90 },
+                        BackgroundColor = new List<string> { "rgba(255, 99, 132, 0.2)" },
+                        BorderColor = new List<string> { "rgb(75, 192, 192)" },
+                        BorderWidth = 1,
+                        Fill = false,
+                        Tension = Convert.ToDecimal( 0.1),
+                         Events=new List<string>{ "click" },
+                        ScaleShowVaEues=true,
+                        Scales=new ChartAxes  {
+                            yAxes = new List<ChartScale> { yAxesScale },
+                            xAxes = new List<ChartScale> { xAxesScale }
+                        },
+                    }
+                },
+            };
+            return lineChart;
+        }
+
+        private vmChart CreateBarChart(string ChartName)
+        {
+            var BarChart = new vmChart
+            {
+                ChartName = ChartName,
+                strType = "bar",
+                Height = "500",
+                Width = "200",
+                Responsive = true,
+                Animation = true,
+                Labels = new List<string> { "2000", "2005", "2010", "2015", "2020" },
+                data = new List<ChartData>
+                {
+                    new ChartData
+                    {
+                        //Label = "My Chart",
+                        Data = new List<double> { 30, 50, 20, 70, 40 },
+                        BackgroundColor = new List<string>
+                        {
+                            "rgba(255, 99, 132, 0.2)",
+                            "rgba(255, 159, 64, 0.2)",
+                            "rgba(255, 205, 86, 0.2)",
+                            "rgba(75, 192, 192, 0.2)",
+                            "rgba(54, 162, 235, 0.2)"
+                        },
+                        BorderColor = new List<string>
+                        {
+                            "rgb(255, 99, 132)",
+                            "rgb(255, 159, 64)",
+                            "rgb(255, 205, 86)",
+                            "rgb(75, 192, 192)",
+                            "rgb(54, 162, 235)"
+                        },
+                        BorderWidth = 1
+                    }
+                },
+                
+            };
+            return BarChart;
+        }
+
+
+        private vmChart CreatePieChart(string ChartName)
+        {
+            var PieChart = new vmChart
+            {
+                ChartName=ChartName,
+                strType = "pie",
+                Height = "150",
+                Width = "200",
+                Responsive = true,
+                Animation = true,
+                Labels = { "2005", "2010", "2015", "2020", "2023" },
+                data = new List<ChartData>
+                {
+                   new ChartData {
+                   //Label = "My Chart",
+                    Data = new List<double> { 30, 50, 20, 70, 40 },
+                    BackgroundColor =  new List<string>() { "rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)" },
+                    BorderColor = new List<string>() { "rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)" },
+                    HoverOffset=4
+                    },
+                },
+
+
+
+
+            };
+            return PieChart;
+        }
+        private vmChart CreateDoughnutChart(string ChartName)
+        {
+            var PieChart = new vmChart
+            {
+                ChartName = ChartName,
+                strType = "doughnut",
+                Height = "150",
+                Width = "200",
+                Responsive = true,
+                Animation = true,
+                Labels = { "2005", "2010", "2015", "2020", "2023" },
+                data = new List<ChartData>
+                {
+                   new ChartData {
+                   //Label = "My Chart",
+                    Data = new List<double> { 30, 50, 20, 70, 40 },
+                      BackgroundColor =new List<string>() { "rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)" },
+                    BorderColor = new List<string>() { "rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)" },
+                    HoverOffset=4
+                    },
+                },
+
+
+
+
+            };
+            return PieChart;
+        }
+        private vmChart CreateScatterChart(string ChartName)
+        {
+            var PieChart = new vmChart
+            {
+                ChartName = ChartName,
+                strType = "scatter",
+                Height = "150",
+                Width = "200",
+                Responsive = true,
+                Animation = true,
+                Labels = { "2005", "2010", "2015", "2020", "2023" },
+                data = new List<ChartData>
+                {
+                   new ChartData {
+                   //Label = "My Chart",
+                    Data = new List<double> { 30, 50, 20, 70, 40 },
+                    BackgroundColor =new List<string>() { "rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)" },
+                    BorderColor = new List<string>() { "rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)" },
+                    HoverOffset=4
+                    },
+                },
+
+
+
+
+            };
+            return PieChart;
+        }
+        private vmChart CreateBubbleChart(string ChartName)
+        {
+            var PieChart = new vmChart
+            {
+                ChartName = ChartName,
+                strType = "bubble",
+                Height = "150",
+                Width = "200",
+                Responsive = true,
+                Animation = true,
+                Labels = { "2005", "2010", "2015", "2020", "2023" },
+                data = new List<ChartData>
+                {
+                   new ChartData {
+                   //Label = "My Chart",
+                    Data = new List<double> { 30, 50, 20, 70, 40 },
+                     BackgroundColor =new List<string>() { "rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)" },
+                    BorderColor = new List<string>() { "rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)" },
+                    HoverOffset=4
+                    },
+                },
+
+
+
+
+            };
+            return PieChart;
+        }
+        private vmChart CreatePolarChart(string ChartName)
+        {
+            var PieChart = new vmChart
+            {
+                ChartName = ChartName,
+                strType = "polarArea",
+                Height = "150",
+                Width = "200",
+                Responsive = true,
+                Animation = true,
+                Labels = { "2005", "2010", "2015", "2020", "2023" },
+                data = new List<ChartData>
+                {
+                   new ChartData {
+                   //Label = "My Chart",
+                    Data = new List<double> { 30, 50, 20, 70, 40 },
+                     BackgroundColor =new List<string>() { "rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)" },
+                    BorderColor = new List<string>() { "rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)" },
+                    HoverOffset=4
+                    },
+                },
+
+
+
+
+            };
+            return PieChart;
+        }
+    }
 }
+
+
+
---
+++ SilkFlo.Web/Models/Charts/vmChartjs.cs
@@ -0,0 +1,137 @@
+﻿using Azure.Storage.Blobs.Models;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace SilkFlo.Web.Models.Charts
+{
+
+    public class vmChart
+    {
+        public string ChartName { get; set; }
+        public vmChart() { Labels = new List<string>(); data = new List<ChartData>(); }
+        public string strType { get; set; }
+        public string Height { get; set; } // Height of the chart container
+        public string Width { get; set; } // Width of the chart container
+        public bool Responsive { get; set; } // Whether the chart should be responsive
+        public bool Animation { get; set; } // Whether to enable animations
+        public ChartLegend Legend { get; set; }
+        public List<string> Labels { get; set; }
+        public List<ChartData> data { get; set; }
+        public ChartOptions options { get; set; }
+        public List<ChartPlugin> Plugins { get; set; }
+    }
+    public class ChartData
+    {
+        public ChartData() { BorderColor = Events= BackgroundColor = new List<string>(); }
+
+        public string Label { get; set; }
+        public List<double> Data { get; set; }
+        public List<string> BackgroundColor { get; set; }
+        public List<string> BorderColor { get; set; }
+        public int BorderWidth { get; set; }
+        public int HoverOffset { get; set; }
+        public bool Fill { get; set; }
+        public decimal Tension { get; set; }
+        public string PointBackgroundColor { get; set; }
+        public string PointBorderColor { get; set; }
+        public string PointHoverBackgroundColor { get; set; }
+        public string PointHoverBorderColor { get; set; }
+        public List<string> Events { get; set; }
+        public bool ScaleShowVaEues { get; set; }
+        public ChartAxes Scales { get; set; }
+    }
+    public class ChartPlugin
+    {
+
+    }
+    public class ChartLegend
+    {
+        public bool Display { get; set; } // Is the legend shown?
+        public string Position { get; set; } // Position of the legend
+        public string Align { get; set; } // Alignment of the legend
+        public int? MaxHeight { get; set; } // Maximum height of the legend, in pixels
+        public int? MaxWidth { get; set; } // Maximum width of the legend, in pixels
+        public bool FullSize { get; set; } // Should take the full width/height of the canvas
+        public string OnClick { get; set; } // JavaScript function name or code for the click event callback
+        public string OnHover { get; set; } // JavaScript function name or code for the hover event callback
+        public string OnLeave { get; set; } // JavaScript function name or code for the leave event callback
+        public bool Reverse { get; set; } // Legend will show datasets in reverse order
+       
+        public bool Rtl { get; set; } // Rendering the legends from right to left
+        public string TextDirection { get; set; } // Force text direction 'rtl' or 'ltr' on the canvas for rendering
+     
+    }
+
+    public class ChartPageViewModel
+    {
+        public ChartPageViewModel()
+        {
+            lstChart = new List<vmChart>();
+        }
+        public List<vmChart> lstChart { get; set; }
+    }
+    public class vmCharts
+    {
+        public vmChart lineChart { get; set; }
+        public vmChart barChart { get; set; }
+        public vmChart pieChart { get; set; }
+        public vmChart doughnutChart { get; set; }
+        public vmChart bubbleChart { get; set; }
+        public vmChart polarChart { get; set; }
+        public vmChart scatterChart { get; set; }
+    }
+
+    public class TypeofChart
+    {
+        public bool lineChart { get; set; }
+        public bool barChart { get; set; }
+        public bool pieChart { get; set; }
+        public bool doughnutChart { get; set; }
+        public bool bubbleChart { get; set; }
+        public bool polarChart { get; set; }
+        public bool scatterChart { get; set; }
+    }
+
+
+    public class ChartOptions
+    {
+        public bool ScaleShowValues { get; set; } // Whether to show scale values
+        public ChartAxes scales { get; set; } // Scale options for axes
+    }
+
+   
+
+   
+
+    
+
+    public class ChartAxes
+    {
+        public List<ChartScale> yAxes  { get; set; } // Options for Y axes
+        public List<ChartScale> xAxes { get; set; } // Options for X axes
+                                                    // Other common scales for all chart types
+    }
+
+    public class ChartScale
+    {
+        public ChartTicks Ticks { get; set; } // Options for scale ticks
+        //public bool beginAtZero { get; set; }                          // Other common scale properties for all chart types
+    }
+
+    public class ChartTicks
+    {
+        public bool BeginAtZero { get; set; } // Whether scale ticks should begin at zero
+                                              // Other common tick properties for all chart types
+    }
+
+   
+
+  
+
+
+
+
+}
+
+
+
